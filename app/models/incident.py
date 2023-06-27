3dfa2ae446e06e82127a9236b656ac8a7557b682
Nate Shoffner <nate.shoffner@gmail.com>
2023-06-26 21:28:42 -0400

    Use ArcGIS for model

--- app/models/incident.py
+++ app/models/incident.py
@@ -2,17 +2,29 @@ import datetime
 from app.models import BaseModel
 from peewee import *
 
+
 class Incident(BaseModel):
-    guid = CharField(primary_key=True)
+    # Incident data
     category = CharField()
     description = CharField()
     intersection = CharField(null=True)
     township = CharField()
     dispatched_at = DateTimeField()
+    number = IntegerField(unique=True)
+    priority = IntegerField(null=True)
+    agency = CharField()
+
+    # coordinates
+    latitude = DecimalField(null=True)
+    longitude = DecimalField(null=True)
+
+    # meta data
     added_at = DateTimeField()
     updated_at = DateTimeField(default=datetime.datetime.utcnow())
     resolved_at = DateTimeField(null=True)
+
+    client = CharField(null=True)
     automatically_resolved = BooleanField(default=False)
 
     class Meta:
-        table_name = 'incidents'
+        table_name = "incidents"
--- app/routers/incidents.py
+++ app/routers/incidents.py
@@ -13,27 +13,33 @@ router = APIRouter()
 
 logger = logging.getLogger(__name__)
 
+
 class ResponseModel(BaseModel):
     message: str
     data: Optional[dict] = None
 
+
 @router.get("/stats")
 async def stats():
-
-    """ Returns various statistics about the API """
+    """Returns various statistics about the API"""
 
     total_incidents = Incident.select().count()
-    total_active_incidents = Incident.select().where(Incident.resolved_at.is_null()).count()
-    total_resolved_incidents = Incident.select().where(Incident.resolved_at.is_null(False)).count()
+    total_active_incidents = (
+        Incident.select().where(Incident.resolved_at.is_null()).count()
+    )
+    total_resolved_incidents = (
+        Incident.select().where(Incident.resolved_at.is_null(False)).count()
+    )
 
     data = {
-        'total_incidents': total_incidents,
-        'total_active_incidents': total_active_incidents,
-        'total_resolved_incidents': total_resolved_incidents,
-        'lcwc_version': get_lcwc_version(),
+        "total_incidents": total_incidents,
+        "total_active_incidents": total_active_incidents,
+        "total_resolved_incidents": total_resolved_incidents,
+        "lcwc_version": get_lcwc_version(),
     }
 
-    return ResponseModel(status=True, message='Success', data=data)
+    return ResponseModel(status=True, message="Success", data=data)
+
 
 @router.get("/incidents")
 async def incidents(
@@ -43,12 +49,12 @@ async def incidents(
     intersection: str = None,
     township: str = None,
     dispacted_at: datetime.date = None,
-    show_related: bool = False):
-
-    """ Returns a list of active incidents """
+    show_related: bool = False,
+):
+    """Returns a list of active incidents"""
 
     incidents = Incident.select().where(Incident.resolved_at.is_null())
-    
+
     if guid:
         incidents = incidents.where(Incident.guid == guid)
     if category:
@@ -61,51 +67,60 @@ async def incidents(
         incidents = incidents.where(Incident.township.contains(township))
     if dispacted_at:
         incidents = incidents.where(Incident.dispatched_at == dispacted_at)
-    
+
     incidents.order_by(Incident.dispatched_at.desc())
 
     data = {
-        'count': len(incidents),
-        'incidents': [model_to_dict(item) for item in incidents]
+        "count": len(incidents),
+        "incidents": [model_to_dict(item) for item in incidents],
     }
 
-    return ResponseModel(status=True, message='Success', data=data)
+    return ResponseModel(status=True, message="Success", data=data)
+
 
 @router.get("/incidents/by-date-range/{start}/{end}")
 async def incident(start: datetime.date, end: datetime.date):
-
-    #list(Event.select().where(fn.date_trunc('day', Event.event_date) == event_date_dt))
-    #incidents = IncidentModel.select().where(IncidentModel.category == IncidentCategory.FIRE)
-    #(fn.date('day', Incident.dispacted_at) == date
-    incidents = Incident.select().where(Incident.dispatched_at.between(start, end)).order_by(Incident.dispatched_at.desc())
+    # list(Event.select().where(fn.date_trunc('day', Event.event_date) == event_date_dt))
+    # incidents = IncidentModel.select().where(IncidentModel.category == IncidentCategory.FIRE)
+    # (fn.date('day', Incident.dispacted_at) == date
+    incidents = (
+        Incident.select()
+        .where(Incident.dispatched_at.between(start, end))
+        .order_by(Incident.dispatched_at.desc())
+    )
 
     data = {
-        'count': len(incidents),
-        'incidents': [model_to_dict(item) for item in incidents]
+        "count": len(incidents),
+        "incidents": [model_to_dict(item) for item in incidents],
     }
 
-    return ResponseModel(status=True, message='Success', data=data)
+    return ResponseModel(status=True, message="Success", data=data)
+
 
 @router.get("/incident/id/{id}")
 async def incident(id: int):
     try:
         incident = Incident.get_by_id(int)
     except Incident.DoesNotExist:
-        raise HTTPException(status_code=404, detail=f"Incident with {id=} does not exist.")
-    return ResponseModel(status=True, message='Success', data=model_to_dict(incident))
+        raise HTTPException(
+            status_code=404, detail=f"Incident with {id=} does not exist."
+        )
+    return ResponseModel(status=True, message="Success", data=model_to_dict(incident))
+
 
 @router.get("/incidents/related/{id}")
 async def incident(id: int):
-
-    incidents = Incident.select().where(Incident.intersection == (Incident.select(Incident.intersection).where(Incident.id == id)))
+    incidents = Incident.select().where(
+        Incident.intersection
+        == (Incident.select(Incident.intersection).where(Incident.id == id))
+    )
 
     incidents.order_by(Incident.dispatched_at.desc())
 
     data = {
-        'intersection': incidents[0].intersection,
-        'count': len(incidents),
-        'incidents': [model_to_dict(item) for item in incidents]
+        "intersection": incidents[0].intersection,
+        "count": len(incidents),
+        "incidents": [model_to_dict(item) for item in incidents],
     }
 
-    return ResponseModel(status=True, message='Success', data=data)
-
+    return ResponseModel(status=True, message="Success", data=data)
