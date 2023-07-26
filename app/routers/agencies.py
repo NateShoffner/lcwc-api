ef1605ec5364ad9d205ee7d0adff520b9880606d
Nate Shoffner <nate.shoffner@gmail.com>
2023-07-26 05:27:56 -0400

    Agencies search

--- app/routers/agencies.py
+++ app/routers/agencies.py
@@ -9,12 +9,50 @@ from pydantic import BaseModel
 from app.models.agency import Agency
 
 agency_router = APIRouter(
-    prefix="/agency",
-    tags=["agency"],
+    prefix="/agencies",
+    tags=["agencies"],
     responses={404: {"description": "Not found"}},
 )
 
 logger = logging.getLogger(__name__)
+
+
+@agency_router.get("/search")
+async def search_agencies(
+    category: Optional[IncidentCategory] = None,
+    station_id: Optional[str] = None,
+    name: Optional[str] = None,
+    url: Optional[str] = None,
+    address: Optional[str] = None,
+    city: Optional[str] = None,
+    state: Optional[str] = None,
+    zip_code: Optional[str] = None,
+    phone: Optional[str] = None,
+):
+    """Search for agencies"""
+
+    agencies = []
+
+    if category:
+        agencies = Agency.select().where(Agency.category == category)
+    if station_id:
+        agencies = Agency.select().where(Agency.station_id == station_id)
+    if name:
+        agencies = Agency.select().where(Agency.name == name)
+    if url:
+        agencies = Agency.select().where(Agency.url == url)
+    if address:
+        agencies = Agency.select().where(Agency.address == address)
+    if city:
+        agencies = Agency.select().where(Agency.city == city)
+    if state:
+        agencies = Agency.select().where(Agency.state == state)
+    if zip_code:
+        agencies = Agency.select().where(Agency.zip_code == zip_code)
+    if phone:
+        agencies = Agency.select().where(Agency.phone == phone)
+
+    return [model_to_dict(agency) for agency in agencies]
 
 
 @agency_router.get("/{category}")
@@ -31,7 +69,7 @@ async def agencies(category: IncidentCategory):
 
 @agency_router.get("/{category}/{id}")
 async def agency(category: IncidentCategory, id: str):
-    """Get a single agency for a given category and id"""
+    """Get a single agency for a given category and ID"""
 
     agency = None
     try:
